fe2d3a859805223a3d0ec91bc6c39a14849f7b90
jaspershen <shenxt1990@outlook.com>
2024-01-13 16:24:44 -0800

    update

--- 1-code/11-correlation_network/1-cross_sectional_network/2-correlation/1-stool_microbiome_vs_omics.R
+++ 1-code/11-correlation_network/1-cross_sectional_network/2-correlation/1-stool_microbiome_vs_omics.R
@@ -13,8 +13,14 @@ setwd(r4projects::get_project_wd())
 load("3-data_analysis/11-correlation_network//1-cross_sectional_network//1-data_preparation/stool_microbiome_data.rda")
 stool_microbiome_data
 
+####metabolome and lipidome
+load("3-data_analysis/11-correlation_network//1-cross_sectional_network/1-data_preparation/metabolomics_data.rda")
+load("3-data_analysis/11-correlation_network//1-cross_sectional_network/1-data_preparation/lipidomics_data.rda")
 
+metabolomics_data
+lipidomics_data
 
+metabolomics_data@variable_info
 
-
+colnames(metabolomics_data)
 
