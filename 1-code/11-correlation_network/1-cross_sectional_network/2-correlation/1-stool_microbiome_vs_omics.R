4bc0daede7fbafe71bb47a5fdace184c2eaff7dd
jaspershen <shenxt1990@outlook.com>
2024-01-14 16:08:27 -0800

    update

--- 1-code/11-correlation_network/1-cross_sectional_network/2-correlation/1-stool_microbiome_vs_omics.R
+++ 1-code/11-correlation_network/1-cross_sectional_network/2-correlation/1-stool_microbiome_vs_omics.R
@@ -26,8 +26,21 @@ colnames(metabolomics_data)
 
 ###get the interseaction of colnames of stool_Microbiome and metabolomics_data and lipidomics_data
 
+intersected_sample_id <-
 intersect(intersect(colnames(stool_microbiome_data),colnames(metabolomics_data)), colnames(lipidomics_data))
 
+intersected_sample_id
 
+####only selection the intersected samples for all three datasets
+stool_microbiome_data <- stool_microbiome_data[,intersected_sample_id]
+metabolomics_data <- metabolomics_data[,intersected_sample_id]
+lipidomics_data <- lipidomics_data[,intersected_sample_id]
 
+dim(stool_microbiome_data)
+dim(metabolomics_data)
+dim(lipidomics_data)
 
+###For microbiome data, we will use the genus level and relative abundance
+x = 
+microbiomedataset::summarise_variables(stool_microbiome_data,
+what = "sum_intensity", group_y = "Genus")
